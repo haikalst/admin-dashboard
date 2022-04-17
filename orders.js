0607fb4f64f9a156a93a0138ca4c32203bcf2250
Haikal Saparudin <haikalst@users.noreply.github.com>
2022-04-17 14:05:12 +0700

    first commit

---
+++ orders.js
@@ -0,0 +1,13 @@
+const Orders = [{
+        productName: 'Phone Holder Portable MK2',
+        productNumber: '8656',
+        productPrice: 'Rp120.000',
+        shipping: 'Dibatalkan'
+    },
+    {
+        productName: 'Phone Holder Portable MK2',
+        productNumber: '8656',
+        productPrice: 'Rp120.000',
+        shipping: 'Diproses'
+    }
+];
